fb954ef8f2b80c81ee0405127b7a58091f196886
minirachel <rachel.a.levinson@gmail.com>
2022-02-21 13:45:30 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const timeSplit = time.split(':');
+  const timeNum = parseInt(timeSplit);
+
+  if (timeNum < 12){
+    return `Good Morning`
+  }
+
+  else if (timeNum > 17){
+    return `Good Evening`
+  }
+
+  else {
+    return `Good Afternoon`
+  }
+}
+
+function displayMessage(string) {
+  document.getElementById("greeting").textContent = string;
+}
+
+
 /* Write your implementation of displayMessage() */
